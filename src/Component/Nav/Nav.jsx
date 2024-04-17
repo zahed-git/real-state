9f92bd1ace521aededf94b8ca765c608c83b9664
zahed-git <mdzahed18@gmail.com>
2024-04-17 16:54:56 +0600

    nav-modifyed

--- src/Component/Nav/Nav.jsx
+++ src/Component/Nav/Nav.jsx
@@ -51,10 +51,9 @@ const Nav = () => {
                 <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                   <div className="w-10 rounded-full">
                     {/* ---------------------------- */}
-                    <button data-tooltip-target="tooltip-bottom" data-tooltip-placement="bottom" ><img alt="Tailwind CSS Navbar component" src={user.photoURL} /></button>
-                    <div id="tooltip-bottom" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
-                      tooltip
-                      <div class="tooltip-arrow" data-popper-arrow></div>
+                    
+                    <div className="tooltip" data-tip="hello">
+                      <img alt="" src={user.photoURL} />
                     </div>
                     {/* ---------------------------- */}
                   </div>
