471e72fd7fcd81fbeed6e73be02f8c868ae214b4
zahed-git <mdzahed18@gmail.com>
2024-07-16 21:43:17 +0600

    added

--- src/Component/Banner/Banner.jsx
+++ src/Component/Banner/Banner.jsx
@@ -1,4 +1,6 @@
-
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import 'swiper/css/bundle'
 import bann1 from '../../images/3d-rendering-810-540 -2.jpg'
 import bann2 from '../../images/3d-rendering-white-810-540-2.jpg'
 import bann3 from '../../images/spacious-810-540.jpg'
@@ -6,33 +8,24 @@ import bann3 from '../../images/spacious-810-540.jpg'
 
 
 const Banner = () => {
-    return (
-        <div className=''>
-            <div className="carousel w-full">
-  <div id="slide1" className="carousel-item relative w-full max-h-[510px]">
-    <img src={bann1} className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide4" className="btn btn-circle">❮</a> 
-      <a href="#slide2" className="btn btn-circle">❯</a>
+  return (
+    <div className='flex'>
+      <div className='container mx-auto'>
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+      >
+        <SwiperSlide><img src={bann1} className="p-5 card shadow w-full " /></SwiperSlide>
+        <SwiperSlide><img src={bann2} className="p-5 card shadow w-full " /></SwiperSlide>
+        <SwiperSlide><img src={bann3} className="p-5 card shadow w-full " /></SwiperSlide>
+
+      </Swiper>
+      </div>
     </div>
-  </div> 
-  <div id="slide2" className="carousel-item relative w-full max-h-[510px]">
-    <img src={bann2} className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide1" className="btn btn-circle">❮</a> 
-      <a href="#slide3" className="btn btn-circle">❯</a>
-    </div>
-  </div> 
-  <div id="slide3" className="carousel-item relative w-full max-h-[510px]">
-    <img src={bann3} className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide2" className="btn btn-circle">❮</a> 
-      <a href="#slide1" className="btn btn-circle">❯</a>
-    </div>
-  </div> 
-</div>
-        </div>
-    );
+  );
 };
 
 export default Banner;
